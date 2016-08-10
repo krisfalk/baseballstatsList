7e5ba46fa34da5005d267713c1565256e0cafd0f
krisfalk <krisfalk2007@hotmail.com>
2016-08-10 16:03:44 -0500

    initial upload of baseball list assignment

---
+++ baseballstatLists/ListTools.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseballstatLists
+{
+    class ListTools
+    {
+        List<string> names;
+        List<double> stats;
+
+        public ListTools()
+        {
+            names = new List<string>();
+            names.Add("At Bats");
+            names.Add("Home Runs");
+            names.Add("RBI");
+            names.Add("Walks");
+            names.Add("Batting Average");
+            stats = new List<double>();
+        }
+        public void PrintStats()
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i] + " " + stats[i]);
+            }
+        }
+        public void askStats()
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine("Enter number of " + names[i] + ":");
+                stats.Add(Convert.ToDouble(Console.ReadLine()));
+            }
+        }
+    }
+}
---
+++ baseballstatLists/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseballstatLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListTools myList = new ListTools();
+            myList.askStats();
+            Console.Clear();
+            myList.PrintStats();
+            Console.ReadLine();
+        }
+    }
+}
